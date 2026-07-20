47b01dd8fc11eb75653b2ac7b25069ca88f4992f
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2026-07-20 17:09:04 +0800

    Day 6 - we built a to do list loop, where it first prompts the user with would you like to add, view or quit the to-do chores list, also learn the flow of a loop

---
+++ projects/todo.py
@@ -0,0 +1,19 @@
+
+chores = [] 
+while True:
+    user_input = input("Would you like to add, view or quit the list? ")
+
+    if user_input == "add": 
+        item = input("What would you like to add? ")
+        chores.append(item)
+    
+    elif user_input == "view":
+        print(chores)
+    
+    elif user_input == "quit":
+        break
+
+    else:
+        print("Invalid input. Please enter 'add', 'view', or 'quit'.") 
+
+
